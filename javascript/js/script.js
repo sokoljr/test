e870819dc4b4c62d91ca922d1f8c365e75224743
Sergey Sokol <serg607.sokol@gmail.com>
2016-06-08 22:57:46 +0300

    js

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,5 +1,19 @@
 /**
  * Created by Home on 06.06.2016.
  */
-alert( 'Я – JavaScript!' );
 
+var userName=prompt('Кто пришел?','');
+if (userName=='Админ') {
+    var pass=prompt('Пароль','');
+    if (pass=='привет') {
+        alert('Добро пожаловать');
+    } else if (pass==null) {
+        alert('Вход отменен');
+    } else {
+        alert("пароль не верный");
+    }
+} else if (userName==null) {
+    alert('Вход отменен');
+} else {
+    alert('Я Вас не знаю');
+}
