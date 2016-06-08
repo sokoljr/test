732a888e7dca4e42d939e622bae14a9d1243c521
sokol <serg607.sokol@gmail.com>
2016-06-08 20:57:46 +0300

    js

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -3,3 +3,36 @@
  */
 alert( 'Я – JavaScript!' );
 
+/** Задача про простые числа от 1 до 10*/
+var min = 1;
+var max = 10;
+
+if (min === 1) {
+  alert(min);
+  min++;
+}
+
+for (var i = min; i <= max; i++) {
+  var simple = true;
+  for (var j = min; j < i; j++) {
+    if (i % j === 0) {
+      simple = false;
+      break;
+    }
+  }
+  if (simple)
+    alert (i);
+} 
+
+
+
+/** пример функции*/
+function toPower(number, power) {   
+	var result = 1;
+	for (i=0; i < power; i++)
+		result = result * number;
+	return result;
+}
+
+var result = toPower(2, 2);
+alert(result);
