f198a3d80e39f4035b559c7cbbcf9fe7600cf65d
Sergey Sokol <serg607.sokol@gmail.com>
2016-06-10 14:29:22 +0300

    js

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,9 +1,4 @@
-/**
- * Created by Home on 06.06.2016.
- */
-
-<<<<<<< HEAD
-var userName=prompt('Кто пришел?','');
+ var userName=prompt('Кто пришел?','');
 if (userName=='Админ') {
     var pass=prompt('Пароль','');
     if (pass=='привет') {
@@ -18,8 +13,9 @@ if (userName=='Админ') {
 } else {
     alert('Я Вас не знаю');
 }
-=======
-/** Задача про простые числа от 1 до 10*/
+
+
+// Задача про простые числа от 1 до 10
 var min = 1;
 var max = 10;
 
@@ -42,7 +38,7 @@ for (var i = min; i <= max; i++) {
 
 
 
-/** пример функции*/
+// пример функции
 function toPower(number, power) {   
 	var result = 1;
 	for (i=0; i < power; i++)
@@ -52,4 +48,64 @@ function toPower(number, power) {
 
 var result = toPower(2, 2);
 alert(result);
+
+
+
+
+// Задача калькулятор
+
+var x = prompt('Введите число?', '');
+var y = prompt('Введите второе число?', '');
+var z = prompt('введите + ; введите - ; введите * ; введите / ', '');
+var result;
+
+function calc () {
+    if (z == '+') {
+        result = (+x) + (+y) ;
+    }
+    else if (z == '-') {
+        result = x - y;
+    }
+    else if (z == '*') {
+        result = x * y;
+    }
+    else if (z == '/') {
+        result = x / y;
+    }
+}
+alert(result);
+
+
+
+
+// Задача калькулятор
+
+var x = prompt('Введите число?', '');
+var y = prompt('Введите второе число?', '');
+var z = prompt('введите 1 - сложение; введите 2 - вычитание; введите 3 - умножение; введите 4 - деление', '');
+var result;
+
+switch(+z)
+{
+    case 1 : result = (+x) + (+y);
+        break;
+    case 2 : result = x - y;
+        break;
+    case 3 : result = x * y;
+        break;
+    case 4: result = x / y
+}
+alert(result);
+
+
+
+
+
+ // цельсий в фаренгейт
+ var c = prompt ('введите значение','');
+ var f;
+ if (c > 0) {
+     f = c * 1.8 + 32;
+ }
+ alert(f);
 
