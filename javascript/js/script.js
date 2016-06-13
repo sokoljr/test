ba6c935f4066b6f9a97affe5ac26ee05bf3aa0b2
sokol <serg607.sokol@gmail.com>
2016-06-13 21:10:26 +0300

    js

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -54,7 +54,7 @@ alert(result);
 
 // Задача калькулятор
 
-var x = prompt('Введите число', '');
+var x = prompt('Введите первое число', '');
 var z = prompt('введите знак - сложение,вычитание,умножение или деление', '');
 var y = prompt('Введите второе число', '');
 var r;
@@ -63,15 +63,15 @@ y = parseFloat(y);
 
 function calc(r) {
    if (isNaN(x)) {
-     alert( "Неккоректно введено первое число" );
+     alert( "Некорректно введено первое число" );
      return null;
    };
    if (z !== "+" && z !== "-" && z !== "*" && z !== "/") {
-     alert("Неккоректно введен знак"); 
+     alert("Некорректно введен знак"); 
      return null;
    };
    if (isNaN(y)) {
-     alert( "Неккоректно введено второе число" );
+     alert( "Некорректно введено второе число" );
      return null;
    };
    if (z === '+') {
@@ -94,35 +94,42 @@ if (result !== null)
 
 // Задача калькулятор
 
-var x = prompt('Введите число', '');
+var x,y;
+x = userInput('Введите первое число');
 var z = prompt('введите знак - сложение,вычитание,умножение или деление', '');
-var y = prompt('Введите второе число', '');
-var r;
+y = userInput('Введите второе число');
+alert(calc(x,y,z));
 
-
-switch(z)
-{
-    case "+" : r = (+x) + (+y);
-        break;
-    case "-" : r = x - y;
-        break;
-    case "*" : r = x * y;
-        break;
-    case "/" :
-    if (y == 0) {
-        alert("Нельзя делить на ноль!");
-    } else {
-        r = x / y;
-    } break;
+function userInput(name) {
+	var number;
+	do {
+		number = (prompt("Enter " + name + " number"));
+		number = (checkNumber(number);
+	}while(number===false);
+	return number;
 }
-alert(r);
+
+function checkNumber(number) {
+	if (isNaN(number) && number==="")
+		return false;
+	return parseFloat(number);
+}
+
+function calc(x,y,z) {
+switch(z) {
+    case "+" : return x + y;
+    case "-" : return x - y;
+    case "*" : return x * y;
+	case "/" : if (y === 0)
+			return "Деление на ноль запрещено!";
+		return x / y;
+	default: return "Z " + z + " is not supported";
+   }
+}
 
 
-
-
-
- // цельсий в фаренгейт
-var celc = prompt ("Введите значиние", "");
+// цельсий в фаренгейт
+var celc = prompt ("Введите значение", "");
 
 function convertFahr (celc) {
   return 1.8 * celc + 32;
@@ -134,12 +141,12 @@ alert(convertFahr (celc));
 // уникальность
 var string = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
 
-function findUnique (string) {
-  alert (string.length); 
-  for (var i = 0; i < string.length; i++) {
+function findUnique(string) {
+  alert(string.length); 
+  for(var i = 0; i < string.length; i++) {
     var letter = string [i];
     var nextIndex = string.indexOf (letter, i++);
-    if (nextIndex === -1) {
+    if(nextIndex === -1) {
       alert(letter);
       break;
     }
@@ -151,3 +158,58 @@ function findUnique (string) {
 
 var min = prompt ("Введите минимальное число", "");
 var max = prompt ("Введите максимальное число", "");
+
+//primer
+
+var obj = {
+  name: "John",
+  height: 185,
+  weight: 75,
+  age: 27,
+  toString: function() {
+    var str = "";
+    for (var key in this) {
+      if (key!== "toString")
+      str += key + ": " + this[key] + " ";
+    }
+    return str;
+  }
+  
+}
+
+var obj2 = {
+  name: "Max",
+  height: 185,
+  weight: 75,
+  age: 27,
+  toString: function() {
+    var str = "";
+    for (var key in this) {
+      if (key!== "toString")
+      str += key + ": " + this[key] + " ";
+    }
+    return str;
+  }
+}
+
+alert(obj + obj2);
+
+// setTimeout
+var counter = setTimeout(function() {
+  alert("after timeout");
+}, 3000);
+
+
+// setInterval
+var counter = setInterval(function() {
+  alert("after timeout");
+}, 3000);
+
+// setTimeout
+var a = 10;
+
+setTimeout(function() {
+  alert(a);
+},1000);
+
+a=5; // выведет 5
