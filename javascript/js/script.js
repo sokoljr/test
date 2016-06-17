bba8fa7b03a2b1bec6a8e44512074511acef1466
sokol <serg607.sokol@gmail.com>
2016-06-17 19:29:18 +0300

    js

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -337,11 +337,11 @@ person7 = {name: "Maya", surname: "Cari", weight: 78, height: 112};
 
 var array = [person1, person2, person3, person4, person5, person6, person7];
 
-function compare(person1, person2) {
-  if(person1.height == person2.height) {
-    return person1.surname > person2.surname;
+function compare(a, b) {
+  if(a.height == b.height) {
+    return a.surname > b.surname;
   } else {
-    return person1.height - person2.height;
+    return a.height - b.height;
     }
 }
 
@@ -350,5 +350,34 @@ array.sort(compare);
 for(var i = 0; i < array.length; i++) {
   alert(array[i].height); 
 }
+
+console.log(array);
+
+// Задача: есть массив объектов (создать вручную, не меньше 4-х объектов, из них должно быть несколько объектов с равными значениями свойства height). Объект описывает человека, есть ключи name, surname, weight, height. Отсортировать "людей" в массиве по росту. Если рост одинаковый - то по фамилии. Использовать метод sort для массивов
+
+var person1 = {name: "Sergey", surname: "Sokol", weight: 95, height: 190},
+person2 = {name: "John", surname: "Huge", weight: 90, height: 190},
+person3 = {name: "Max", surname: "Kill", weight: 87, height: 167},
+person4 = {name: "Mikel", surname: "Rodzinskii", weight: 78, height: 175},
+person5 = {name: "Maria", surname: "Luna", weight: 32, height: 190},
+person6 = {name: "Alice", surname: "Bora", weight: 45, height: 132},
+person7 = {name: "Maya", surname: "Cari", weight: 78, height: 112};
+
+var array = [person1, person2, person3, person4, person5, person6, person7];
+
+function compare(a, b) {
+  if(a.height !== b.height) 
+    return a.height - b.height;
+var surnameA = a.surname.toLowerCase();
+var surnameB = b.surname.toLowerCase();
+  if (surnameA > surnameB) 
+    return 1;
+else if (surnameA < surnameB)
+	return -1;
+else
+	return 0;
+}
+
+array.sort(compare);
 
 console.log(array);
