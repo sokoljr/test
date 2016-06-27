c338489236fbda52080cc5de25135a2cd32b5423
Sergey Sokol <serg607.sokol@gmail.com>
2016-06-27 16:05:21 +0300

    lab_in_class

--- LabInClass/js/script.js
+++ LabInClass/js/script.js
@@ -1,16 +1,15 @@
-window.addEventListener('DOMContentLoaded', init);
-
-function init() {
+function sum() {
 
 	var buttons = document.getElementsByTagName('button');
 	var div = document.querySelector('div');
-	var i;
-
+	
 	for (i = 0; i < buttons.length; i++) {
 		buttons[i].addEventListener('click', function() {
 			div.innerText = +this.innerText + +div.innerText; });
 		};
 }
+
+sum();
 
 
 
