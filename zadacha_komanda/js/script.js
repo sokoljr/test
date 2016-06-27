89bfd754fbf15d6d48b82e6e21c9e01d0f2c86e6
sokol <serg607.sokol@gmail.com>
2016-06-27 11:47:57 +0300

    js

---
+++ zadacha_komanda/js/script.js
@@ -0,0 +1,13 @@
+var buttons = document.getElementsByTagName('button');
+var sum = document.getElementsByClassName("sum");
+
+buttons[0].onclick = function(e) {
+    alert(this.textContent);
+}
+
+
+
+
+
+
+
