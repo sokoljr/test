6dd28e383011b5a863e55f013fbc40ff7a1c2e6d
Sergey Sokol <serg607.sokol@gmail.com>
2016-06-29 15:53:27 +0300

    dz

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -413,3 +413,32 @@ input.oninput = function (e){
 	div.innerHTML = char;
 };
 
+
+// есть select с названиями тегов, textarea и кнопка. 
+// При нажатии кнопки создавать выбранный в select тег, добавлять в него текстовое содержимое из textarea. 
+// Если в textarea есть текст, то создавать элемент так же при переключении select
+var elem,
+	submit = document.getElementById('submit');
+
+function handler(){
+	 var select = document.getElementById('select').value;
+	 var textarea = document.getElementById('textarea').value;
+
+	if (select === 'div'){
+		elem = document.createElement('div');
+	} else if (select === 'p'){
+		elem = document.createElement('p');
+	} else {
+		elem = document.createElement('span');
+	}
+	elem.innerHTML = textarea;
+	document.getElementById('new_div').appendChild(elem);
+};
+function handler2(){
+	var textarea = document.getElementById('textarea').value;
+	if(textarea !== ''){
+		return handler();
+	}	
+}
+submit.addEventListener('click', handler);
+select.addEventListener('change', handler2);
